068493f9297c89751c6ff5cdc26c2b9d0806f1b0
hypOG <117787184+hypOG@users.noreply.github.com>
2024-11-15 11:53:30 +0000

    Add files via upload

---
+++ javascript/calculadora/index.js
@@ -0,0 +1,22 @@
+//CALCULADORA
+
+const decreasebtn = document.getElementById("decreasebtn");
+const increasebtn = document.getElementById("increasebtn");
+const resetbtn = document.getElementById("resetbtn");
+const countlabel = document.getElementById("countlabel");
+let count = 0;
+
+increasebtn.onclick = function() {
+    count++;
+    countlabel.textContent = count;
+}
+
+decreasebtn.onclick = function() {
+    count--;
+    countlabel.textContent = count;
+}
+
+resetbtn.onclick = function() {
+    count = 0 ;
+    countlabel.textContent = count;
+}
